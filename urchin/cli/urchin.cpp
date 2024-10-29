a8d2fb8154061e6a919845236d67879a48cad8c2
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-10-28 23:57:31 -0700

    Setup basic makefile and debug log

---
+++ urchin/cli/urchin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+#ifdef URCHIN_DEBUG
+  std::cout << "[DEBUG] Urchin CLI called with " << argc << " arguments: [";
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << ", ";
+  }
+  std::cout << "." << std::endl;
+#endif
+}
