710b14114957fa348b8c6c0f655d54e73845fdd6
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-11-01 01:50:52 -0700

    Make stop timer return timestamp event string

--- urchin/core/timer.h
+++ urchin/core/timer.h
@@ -2,10 +2,12 @@
 #define URCHIN_TIMER
 
 #include <iomanip>
+#include <ios>
 #include <iostream>
 #include <chrono>
 #include <ratio>
 #include <string>
+#include <strstream>
 
 namespace UrchinCore {
 
@@ -13,10 +15,11 @@ class Timer {
 public:
   Timer(const std::string functionName) : _functionName(functionName), _startTime(std::chrono::high_resolution_clock::now()) {}
 
-  void stop() {
+  std::string stop() {
     this->_endTime = std::chrono::high_resolution_clock::now();
     auto durationMS = std::chrono::duration<double, std::milli>(this->_endTime - this->_startTime);
 
+  #ifdef URCHIN_DEBUG
     std::cout << std::fixed << std::setprecision(2);
     if (durationMS.count() >= 100) {
       auto durationSec = std::chrono::duration_cast<std::chrono::seconds>(this->_endTime - this->_startTime);
@@ -24,6 +27,18 @@ public:
     } else {
       std::cout << "[DEBUG] " << this->_functionName << " took " << durationMS.count() << " milliseconds to execute." << std::endl;
     }
+  #endif // URCHIN_DEBUG
+    
+    std::ostrstream durationMessage;
+    if (durationMS.count() >= 100) {
+      auto durationSec = std::chrono::duration_cast<std::chrono::seconds>(this->_endTime - this->_startTime);
+
+      durationMessage << std::fixed << std::setprecision(2) << this->_functionName << " took " << durationSec.count() << " seconds to execute." << std::endl;
+    } else {
+      durationMessage << std::fixed << std::setprecision(2) << this->_functionName << " took " << durationMS.count() << " milliseconds to execute." << std::endl;
+    }
+
+    return durationMessage.str();
   }
 
 private:
