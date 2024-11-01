c1c21876bd25303c85224719704b83b3359ba33e
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-11-01 01:51:39 -0700

    Add basic log statements to graph command

--- urchin/cli/commands/command.cpp
+++ urchin/cli/commands/command.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <algorithm>
 
+#include "../../core/logger.h"
 #include "command.h"
 #include "helpCommand.h"
 #include "graphCommand.h"
@@ -40,15 +41,19 @@ std::unique_ptr<UrchinCLI::Command> UrchinCLI::CommandParser::findCommand() {
 void UrchinCLI::GraphCommand::execute() {
 #ifdef URCHIN_DEBUG
   std::cout << "[DEBUG] Running graph command." << std::endl;
-  UrchinCore::Timer cmdTimer("Graph Command");
 #endif // URCHIN_DEBUG
+
+  UrchinCore::Timer cmdTimer("Graph Command");
+  _logger.logToFile(UrchinCore::LogLevel::INFO, "Running graph command.");
 
   /* If no filePath argument is provided, Urchin will perform a recursive search from where the command was called. */
   if (_filePath.empty()) {
+    _logger.logToFile(UrchinCore::LogLevel::INFO, "No filePath argument provided. Performing recursive search from working directory.");
     for (const auto &entry : std::filesystem::recursive_directory_iterator(std::filesystem::current_path())) {
       if (entry.is_regular_file()) {
         auto path = entry.path();
         if (path.extension() == ".h" || path.extension() == ".hpp" || path.extension() == ".cpp") {
+          _logger.logToFile(UrchinCore::LogLevel::INFO, std::string("Found file: " + path.string()));
           findDependencies(path.string());
         }
       }
@@ -70,9 +75,7 @@ void UrchinCLI::GraphCommand::execute() {
   }
   std::cout << "]" << std::endl;
 
-#ifdef URCHIN_DEBUG
-  cmdTimer.stop();
-#endif // URCHIN_DEBUG
+  _logger.logToFile(UrchinCore::LogLevel::INFO, cmdTimer.stop());
 }
 
 void UrchinCLI::GraphCommand::findDependencies(const std::string &filePath) {
@@ -83,6 +86,7 @@ void UrchinCLI::GraphCommand::findDependencies(const std::string &filePath) {
   _visited.insert(filePath);
   std::ifstream file(filePath);
   if (!file) {
+    _logger.logToFile(UrchinCore::LogLevel::ERROR, "Couldn't open file to scan for dependencies. Stopping command.");
     std::cerr << "[ERROR] Could not open " << filePath << "." << std::endl; 
     return;
   }
